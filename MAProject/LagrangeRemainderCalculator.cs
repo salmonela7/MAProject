14819141f951b2a38cf67b37e816f90352fc5882
Vingis Simonas <simonas.vingis@mif.stud.vu.lt>
2020-11-28 21:00:52 +0200

    Upgraded remainder calculation

--- MAProject/LagrangeRemainderCalculator.cs
+++ MAProject/LagrangeRemainderCalculator.cs
@@ -10,18 +10,34 @@ namespace MAProject
     {
         public static double CalculateLagrangeRemainder(Expr function, string variableName, int n, FloatingPoint x, FloatingPoint x0)
         {
+            Console.WriteLine("Max: " + Math.Max(x.RealValue, x0.RealValue));
             var variable = new Dictionary<string, FloatingPoint>
             {
-                { variableName, GetRandomNumberInRange(x0, x) }
+                { variableName, Math.Max(x.RealValue, x0.RealValue) }
             };
 
-            var factorial = (FloatingPoint)SpecialFunctions.Factorial(n);
+            var factorial = (FloatingPoint)SpecialFunctions.Factorial(n+1);
 
-            var derivativeResult = function.CalculateFunctionAtDerivative(variable, Expr.Variable(variableName), ++n);
+            var derivativeResult = function.CalculateFunctionAtDerivative(variable, Expr.Variable(variableName), n+1);
 
-            var result = derivativeResult.RealValue / factorial.RealValue * Math.Pow(x.RealValue - variable[variableName].RealValue, n) * (x.RealValue - x0.RealValue);
+            var result = derivativeResult.RealValue / factorial.RealValue * Math.Pow(Math.Abs(x.RealValue - x0.RealValue), n+1);
 
             return result;
+        }
+
+        public static double CalculateExactRemainder(Expr func, FloatingPoint point, int order)
+        {
+            var pointVar = new Dictionary<string, FloatingPoint>
+            {
+                { "x", point}
+            };
+
+            var taylorExpr1 = GetTaylorExpression(order + 1, Expr.Variable("x"), 0, func);
+
+            var trueValue = func.Evaluate(pointVar);
+            var taylorValue = taylorExpr1.Evaluate(pointVar);
+
+            return trueValue.RealValue - taylorValue.RealValue;
         }
 
         public static Expr GetDerivativeOfDegree(
@@ -35,6 +51,22 @@ namespace MAProject
             }
 
             return symbolicFunction;
+        }
+
+        public static Expr GetTaylorExpression(int k, Expr symbol, Expr al, Expr xl)
+        {
+            int factorial = 1;
+            Expr accumulator = Expr.Zero;
+            Expr derivative = xl;
+            for (int i = 0; i < k; i++)
+            {
+                var subs = derivative.Substitute(symbol, al);
+                derivative = derivative.Differentiate(symbol);
+                accumulator = accumulator + subs / factorial * ((symbol - al).Pow(i));
+                factorial *= (i + 1);
+            }
+
+            return accumulator.Expand();
         }
 
         private static FloatingPoint CalculateFunctionAtDerivative(
